7d1ea79f335773b599d0f5ebae8272c00c4db61c
Alvin Lindstam <alvin.lindstam@sweco.se>
2014-10-01 11:14:39 +0200

    Added dynamic behaviour

--- script/test.js
+++ script/test.js
@@ -1,3 +1,19 @@
 $( document ).ready(function() {
-  
+  $('p').on(
+  	'click', 
+  	function(event){  		
+  		$(this).toggleClass('orange')
+  });
+
+
+
+  	$.getJSON( "https://api.github.com/repos/JohanEks/testrepo/commits", function( data ) {
+		  var items = [];
+		  $.each( data, function( key, val ) {
+		  	items.push( "<li id='" + val.sha + "'>"+val.commit.author.name+": \"" + val.commit.message + "\"</li>" );
+		  });
+		 
+			$('ul#commits').html(items.join(""))
+		  
+	});
 });
